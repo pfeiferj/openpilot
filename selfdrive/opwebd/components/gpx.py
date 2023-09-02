05a22da795001a2fc8a95da752cef82350bf4751
Jacob Pfeifer <jacob@pfeifer.dev>
2023-09-02 18:06:27 -0400

    optimize full route gpx

--- selfdrive/opwebd/components/gpx.py
+++ selfdrive/opwebd/components/gpx.py
@@ -73,25 +73,43 @@ def route_gpx(app: Flask):
     dirs = [d for d in os.listdir(SEGMENTS_DIR) if d.startswith(route)]
     dirs.sort(key=sortSegments)
 
-    all_points = []
+    file_descriptors = []
+    point_generators = []
     for d in dirs:
       full_path = os.path.join(SEGMENTS_DIR, d, "rlog")
       file = open(full_path, "rb")
       events = log.Event.read_multiple(file)
-      filtered_events = [e for e in events if 'liveLocationKalman' == e.which()]
-      filtered_events = [e.to_dict() for e in filtered_events]
-      filtered_events = [e for e in filtered_events if valid_location(e)]
-      file.close()
+      filtered_events = enumerate(filter(llkFilter, events))
+      filtered_events = filter(valid_location, filtered_events)
+      filtered_events = map(enumEventDictMap, filtered_events)
 
-      file = open(full_path, "rb")
-      events = log.Event.read_multiple(file)
-      filtered_clocks = [e for e in events if 'clocks' == e.which()]
-      clock = filtered_clocks[0].to_dict()
+      file2 = open(full_path, "rb")
+      events = log.Event.read_multiple(file2)
+      clock = next(e for e in events if 'clocks' == e.which())
+      clock = clock.to_dict()
       offset = mono_offset(clock)
+
+      def map_points(event, offset = offset):
+        dt = mono_datetime(event, offset)
+        bearing_rad = event["liveLocationKalman"]["calibratedOrientationNED"]["value"][2]
+        return {
+          "latitude": event["liveLocationKalman"]["positionGeodetic"]["value"][0],
+          "longitude": event["liveLocationKalman"]["positionGeodetic"]["value"][1],
+          "elevation": event["liveLocationKalman"]["positionGeodetic"]["value"][2],
+          "bearing": math.degrees(bearing_rad),
+          "timestamp": dt.isoformat() + "Z",
+        }
+
+      points = map(map_points, filtered_events)
+      point_generators.append(points)
+      file_descriptors.append(file)
+      file_descriptors.append(file2)
+
+    all_points = list(chain(*point_generators))
+
+    for file in file_descriptors:
       file.close()
 
-      points = [make_point(e, offset) for e in filtered_events]
-      all_points += points
     return Response(render_template('gpx/segment.gpx', points=all_points), mimetype='application/octet-stream')
 
   @app.route("/gpx/route/lite/<route>.gpx")
