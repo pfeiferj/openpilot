197b9f79f8819f0c2b2a94d6e24e504d734f39db
Jacob Pfeifer <jacob@pfeifer.dev>
2024-05-11 11:49:46 -0400

    maybe?

--- selfdrive/car/ford/fordcan.py
+++ selfdrive/car/ford/fordcan.py
@@ -131,14 +131,14 @@ def create_acc_msg(packer, CAN: CanBus, long_active: bool, gas: float, accel: fl
   """
   decel = accel < 0 and long_active
   values = {
-    "AccBrkTot_A_Rq": float(lh.target_accel) if long_active else accel,                          # Brake total accel request: [-20|11.9449] m/s^2
+    "AccBrkTot_A_Rq": accel,                          # Brake total accel request: [-20|11.9449] m/s^2
     "Cmbb_B_Enbl": 1 if long_active else 0,           # Enabled: 0=No, 1=Yes
     "AccPrpl_A_Rq": gas,                              # Acceleration request: [-5|5.23] m/s^2
     "AccPrpl_A_Pred": float(lh.target_accel) if long_active else -5.0,                           # Acceleration request: [-5|5.23] m/s^2
     "AccResumEnbl_B_Rq": 1 if long_active else 0,
     "AccVeh_V_Trg": int(lh.target_speed * CV.MS_TO_KPH) if long_active else v_ego_kph,                        # Target speed: [0|255] km/h
     # TODO: we may be able to improve braking response by utilizing pre-charging better
-    "AccBrkPrchg_B_Rq": 1 if lh.precharge and long_active else 0,            # Pre-charge brake request: 0=No, 1=Yes
+    "AccBrkPrchg_B_Rq": 1 if decel and lh.precharge else 0,            # Pre-charge brake request: 0=No, 1=Yes
     "AccBrkDecel_B_Rq": 1 if decel else 0,            # Deceleration request: 0=Inactive, 1=Active
     "AccStopStat_B_Rq": 1 if stopping else 0,
   }
